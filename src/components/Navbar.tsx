d57c18665e0aa10c05d347d13a3b46b092dc8a27
Misbah Ansori <misbah.ansori24@gmail.com>
2023-06-25 09:22:04 +0700

    feat : add banner and fix navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,44 +1,69 @@
-import React, { useEffect, useState } from "react";
 import { ShoppingCart } from "lucide-react";
 import Link from "next/link";
-import { Router } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
   const [activeItem, setActiveItem] = useState(0);
 
-  
   useEffect(() => {});
   return (
-    <div className="flex items-center justify-between rounded-lg bg-gray-800 p-6">
-      <div className="flex gap-2 items-center">
-        <p className="capitalize font-bold">Food Nirvana</p>
-        <p className="uppercase text-yellow-400 font-bold p-1 px-3 border-2 border-yellow-400 rounded-lg">
+    <div className="mt-8 flex items-center justify-between rounded-lg bg-gray-800 px-6 py-4">
+      <div className="flex items-center gap-2">
+        <p className="font-bold capitalize">Food Nirvana</p>
+        <p className="rounded-lg border-2 border-yellow-400 p-1 px-3 text-xs font-bold uppercase text-yellow-400">
           pro
         </p>
       </div>
       <div>
-        <ul className="flex gap-2 items-center">
+        <ul className="flex items-center gap-1">
           <li>
-            <Link href="/">Home</Link>
+            <Link
+              className="rounded-lg px-4 py-2 text-sm font-bold transition-colors duration-200 hover:bg-yellow-400 hover:text-black"
+              href="/"
+            >
+              Home
+            </Link>
           </li>
           <li>
-            <Link href="/products">Products</Link>
+            <Link
+              className="rounded-lg px-4 py-2 text-sm font-bold transition-colors duration-200 hover:bg-yellow-400 hover:text-black"
+              href="/products"
+            >
+              Products
+            </Link>
           </li>
           <li>
-            <Link href="/reviews">Reviews</Link>
+            <Link
+              className="rounded-lg px-4 py-2 text-sm font-bold transition-colors duration-200 hover:bg-yellow-400 hover:text-black"
+              href="/reviews"
+            >
+              Reviews
+            </Link>
           </li>
           <li>
-            <Link href="/contacts">Contacts</Link>
+            <Link
+              className="rounded-lg px-4 py-2 text-sm font-bold transition-colors duration-200 hover:bg-yellow-400 hover:text-black"
+              href="/contacts"
+            >
+              Contacts
+            </Link>
           </li>
           <li>
-            <Link href="/faq">FAQ</Link>
+            <Link
+              className="rounded-lg px-4 py-2 text-sm font-bold transition-colors duration-200 hover:bg-yellow-400 hover:text-black"
+              href="/faq"
+            >
+              FAQ
+            </Link>
           </li>
         </ul>
       </div>
-      <div className="flex gap-2 items-center">
-        <ShoppingCart />
-        <span>0</span>
-        <button className="capitalize p-4 bg-yellow-400 text-black font-bold rounded-2xl">
+      <div className="flex items-center gap-4">
+        <button type="button" className="flex items-center gap-1">
+          <ShoppingCart />
+          <span>0</span>
+        </button>
+        <button className="rounded-xl bg-yellow-400 px-4 py-2 text-sm font-bold capitalize text-black">
           customer login
         </button>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      colors: {
+        gray: {
+          800: "#1b1b1b",
+          900: "#121212",
+        },
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
+};
