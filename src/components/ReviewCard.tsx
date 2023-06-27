0b58033acfbf85964ab82b293af693195c7cdd59
caesar003 <caesarmuksid@gmail.com>
2023-06-27 15:51:29 +0700

    fix: hide reply container on review when reply text is empty

--- src/components/ReviewCard.tsx
+++ src/components/ReviewCard.tsx
@@ -2,7 +2,6 @@ import { Check } from "lucide-react";
 import { twMerge } from "tailwind-merge";
 import Stars from "./Stars";
 
-
 interface ReviewItem {
   date: string;
   stars: number;
@@ -36,13 +35,17 @@ export default function ReviewCard({ item, className }: PropsInterface) {
             {text}
           </p>
         </div>
-        <div className="my-4 flex gap-2 rounded-lg bg-gray-900 p-2">
-          <div className="border-1 border-l border-yellow-400"></div>
-          <div className="py-2 pl-2">
-            <p className="text-sm font-semibold text-white">{reply && reply}</p>
-            <p className="my-2 mt-4 text-sm">— reply from Food Nirvana</p>
+        {reply ? (
+          <div className="my-4 flex gap-2 rounded-lg bg-gray-900 p-2">
+            <div className="border-1 border-l border-yellow-400"></div>
+            <div className="py-2 pl-2">
+              <p className="text-sm font-semibold text-white">{reply}</p>
+              <p className="my-2 mt-4 text-sm">— reply from Food Nirvana</p>
+            </div>
           </div>
-        </div>
+        ) : (
+          ""
+        )}
         <div className="flex items-center gap-2">
           {isVerified ? (
             <>
